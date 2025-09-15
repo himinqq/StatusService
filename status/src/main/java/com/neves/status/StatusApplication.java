e7be9523f482c7000c75315d03713886bbb150bf
donggyu <leedong1848@gmail.com>
2025-09-15 10:53:55 +0900

    build: 프로젝트 시작

---
+++ status/src/main/java/com/neves/status/StatusApplication.java
@@ -0,0 +1,13 @@
+package com.neves.status;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatusApplication.class, args);
+	}
+
+}
